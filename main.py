a020b204929dfc93e36bce23aae62e746e865bc7
Chris Milazzo <Christophermilazzo@hotmail.com>
2023-09-02 07:37:59 -0400

    D24: Add lines

--- main.py
+++ main.py
@@ -13,20 +13,23 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     #set footer
-    pdf.ln(265)
-    pdf.set_font(family="Times", style="I", size=12)
+    pdf.ln(266)
+    pdf.set_font(family="Times", style="I", size=10)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=10, txt=row["Topic"], align='R')
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
         #set footer
-        pdf.ln(277)
-        pdf.set_font(family="Times", style="I", size=12)
+        pdf.ln(278)
+        pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=10, txt=row["Topic"], align='R')
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
